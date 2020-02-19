eb4f6ff0165e6c13418ded49436506e21ce93675
GMusb <musbmg@gmail.com>
2020-02-19 09:53:04 -0300

    Atualizando classe Hospital e adicionando a classe ProntoSocorro

--- src/aula1202/Hospital.java
+++ src/aula1202/Hospital.java
@@ -4,8 +4,17 @@ import java.util.ArrayList;
 
 public class Hospital {
     private int idHospital;
-    private ArrayList<Funcionario> funcionarios;
+    private ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
     private String endereco;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public int getIdHospital() {
         return idHospital;
@@ -23,5 +32,29 @@ public class Hospital {
         this.endereco = endereco;
     }
     
+    public void addFuncionario(Funcionario funcionario){
+        this.funcionarios.add(funcionario);
+    }
+    public void listarTodosFuncionarios(){
+        funcionarios.forEach(funcionarios->System.out.println(funcionarios));
+    }
+    public void listarMedicos(){
+        for(int i = 0; i < funcionarios.size(); i++){
+            if(funcionarios.get(i) instanceof Medico){
+                System.out.println(funcionarios.get(i));
+            }
+        }
+    }
+    public void listarEnfermeiros(){
+        for(int i = 0; i < funcionarios.size(); i++){
+            if(funcionarios.get(i) instanceof Enfermeiro){
+                System.out.println(funcionarios.get(i));
+            }
+        }
+    }
     
+    public Hospital(int id, String end){
+        this.idHospital = id;
+        this.endereco = end;
+    }
 }
